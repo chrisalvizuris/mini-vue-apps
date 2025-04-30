f2a6e74134ad1a48422ced79afde732374528cd0
christian-alvizuris-VA <122404200+christian-alvizuris-VA@users.noreply.github.com>
2025-04-30 15:50:54 -0700

    Added more return data

--- GoalTracker/app.js
+++ GoalTracker/app.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             courseGoal: 'Finish the course',
-            sdccLink: 'https://comic-con.org'
+            sdccLink: 'https://comic-con.org',
+            otherLink: ''
         };
     }
 });
